6315f69d46b170a60738b8d9fda75a2d6eef45d8
Midemee <ashiwajuaramide@gmail.com>
2026-05-28 15:21:42 +0100

    Update

--- DAY TWO/JAVA/Book Suggestion System/BookSystem.java
+++ DAY TWO/JAVA/Book Suggestion System/BookSystem.java
@@ -1,7 +1,9 @@
 import java.util.Random;
 public class BookSystem{
-
-     public static String getSuggestedBooks(String [] books){
+      public static String [] books = {"Solo", "SuperIntelligence", "India After Ghandi", "Dream Count", "Gaza", "Ripple", "Option B", "The Attributes", "Animal Farm"};
+     
+     //public static void main(String[] args)   
+     public static String getSuggestedBooks(){
      Random randomPick = new Random();
      int bookChoice = randomPick.nextInt(books.length);
      
@@ -14,4 +16,41 @@ public class BookSystem{
      int pageNumber = randomPick.nextInt(1,101);  
      return pageNumber;
     }
+    
+    public static String [] addBooks(String newBook){
+    String [] newBookList = new String[books.length + 1];
+        int newIndex = 0;
+        for(int index = 0; index < books.length; index ++){
+        newBookList[newIndex] = books[index];
+        newIndex++;
+        }
+        newBookList[newIndex] = newBook;
+     return newBookList;
+    }
+    
+    public static String [] removeBooks(String bookName){
+    String [] newBookList = new String[books.length - 1];
+    
+        int newIndex = 0;
+        for(int index = 0; index < books.length; index ++){
+            if(!books[index].equals(bookName)){
+                newBookList[newIndex] = books[index];
+                newIndex++;        
+            }
+        }
+     return newBookList;
+    }
+    
+    public static String [] updateBooks(String oldBookName, String newBookName){
+        for(int index = 0; index < books.length; index++){
+            if (books[index].equals(oldBookName)){
+                books[index] = newBookName;
+            }
+        }
+        return books;
+    }
+    
+    public static String[] showBooks(){
+        return books;
+    }    
 }
--- DAY TWO/JAVA/Book Suggestion System/BookSystemTest.java
+++ DAY TWO/JAVA/Book Suggestion System/BookSystemTest.java
@@ -5,8 +5,7 @@ public class BookSystemTest{
 
     @Test
     public void testThatSuggestedBookIsFromTheList(){
-    String [] bookList = {"Solo", "SuperIntelligence", "India After Ghandi", "Dream Count", "Gaza", "Ripple", "hidden Genius", "Option B", "The Attributes", "Animal Farm"};
-    String bookChoice = BookSystem.getSuggestedBooks(bookList);
+    String bookChoice = BookSystem.getSuggestedBooks();
     assertNotNull(bookChoice);
     System.out.println("Suggested Book: " + bookChoice);
     }     
@@ -18,4 +17,36 @@ public class BookSystemTest{
     assertTrue(pageNumber <= 100);
     System.out.println("Page Number: " + pageNumber);
     }
+    
+    @Test
+    public void testThatANewBookIsAdded(){
+    String newBook = "Hidden Genius";
+    String[] actual = BookSystem.addBooks(newBook);
+    String[] result = {"Solo", "SuperIntelligence", "India After Ghandi", "Dream Count", "Gaza", "Ripple", "Option B", "The Attributes", "Animal Farm", "Hidden Genius"};
+    assertArrayEquals(actual, result);
+    }
+    
+    @Test
+    public void testThatABookIsremoved(){
+    String bookName = "Solo";
+    String[] actual = BookSystem.removeBooks(bookName);
+    String[] result = {"SuperIntelligence", "India After Ghandi", "Dream Count", "Gaza", "Ripple", "Option B", "The Attributes", "Animal Farm"};
+    assertArrayEquals(actual, result);
+    }
+    
+    @Test
+    public void testThatBookNameIsUpdatedAfterChanges(){
+    String oldBookName = "Animal Farm";
+    String newBookName = "Animal Kingdom";
+    String[] actual = BookSystem.updateBooks(oldBookName, newBookName);
+    String[] result = {"Solo", "SuperIntelligence", "India After Ghandi", "Dream Count", "Gaza", "Ripple", "Option B", "The Attributes", "Animal Kingdom"};
+    assertArrayEquals(actual, result);
+    }
+    
+    @Test
+    public void testThatBooksIsShown(){
+    String[] actual = BookSystem.showBooks();
+    String[] result = {"Solo", "SuperIntelligence", "India After Ghandi", "Dream Count", "Gaza", "Ripple", "Option B", "The Attributes", "Animal Kingdom"};
+    assertArrayEquals(actual, result);
+    }      
 }
