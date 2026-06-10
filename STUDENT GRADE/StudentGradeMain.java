a9062dcdb2794b727ece65beb6ced4273750b688
Midemee <ashiwajuaramide@gmail.com>
2026-06-10 01:57:27 +0100

    Update

--- STUDENT GRADE/StudentGradeMain.java
+++ STUDENT GRADE/StudentGradeMain.java
@@ -35,14 +35,14 @@ public class StudentGradeMain{
     double[] averages = StudentGrade.calculateAverage(totals, scores[0].length);
     int[] positions = StudentGrade.findPositions(totals);
 
-    System.out.println("\n==========================================================");
+    System.out.println("\n====================================================================");
     System.out.print("STUDENT\t\t");
 
     for(int subject = 0; subject < scores[0].length; subject++){
         System.out.print("SUB" + (subject + 1) + "\t");
     }
     System.out.println("TOT\tAVG\tPOS");
-    System.out.println("============================================================");
+    System.out.println("======================================================================");
 
     for(int student = 0; student < scores.length; student++){
         System.out.print("Student " + (student + 1) + "\t");
@@ -54,8 +54,8 @@ public class StudentGradeMain{
         System.out.printf("%d\t%.2f\t%d%n", totals[student], averages[student], positions[student]);
     }
 
-    System.out.println("==============================================================");
-    System.out.println("==============================================================");
+    System.out.println("========================================================================");
+    System.out.println("========================================================================");
     
     System.out.println("\nSUBJECT SUMMARY");
     for(int subject = 0; subject < scores[0].length; subject++){
--- STUDENT GRADE/StudentGradeTest.java
+++ STUDENT GRADE/StudentGradeTest.java
@@ -4,21 +4,21 @@ import static org.junit.jupiter.api.Assertions.*;
 public class StudentGradeTest{
 
     @Test
-    public void testThatTotalOfStudentScoresOnAllSubjectsGivesAccurateResult(){
+    public void testThatIfIPassAStudentScoresOnAllSubjectsItWillGivesCorrectTotal(){
         int[][] scores = {{65, 83, 40}};
         int[] result = StudentGrade.calculateTotal(scores);
         assertEquals(188, result[0]);
     }
 
     @Test
-    public void testThatTotalOfStudentScoresWithZerosGivesAccurateResult(){
+    public void testThatTotalOfStudentScoresWithZerosGivesCorrectTotal(){
         int[][] scores = {{75, 0, 42}};
-        int[] result = StudentGrade.calculateTotal(scores);
+        int[] result   = StudentGrade.calculateTotal(scores);
         assertEquals(117, result[0]);
     }
 
     @Test
-    public void testThatTotalsAreCorrectForMultipleStudents(){
+    public void testThatIfIPassMultipleStudentScoresOnAllSubjectsItWillGivesCorrectTotalForEachStudent(){
         int[][] scores = {{65, 83, 40}, {75, 0, 42}};
         int[] result = StudentGrade.calculateTotal(scores);
         assertEquals(188, result[0]);
@@ -26,7 +26,7 @@ public class StudentGradeTest{
     }
 
     @Test
-    public void testThatAverageOfStudentScoresOnAllSubjectsGivesAccurateResult(){
+    public void testThatIfIpassTotalScoreForOneStudentItWillGiveTheCorrectAverage(){
         int[] totals = {220};
         double[] averages = StudentGrade.calculateAverage(totals, 3);
         assertEquals(73.33, averages[0], 0.01);
@@ -34,7 +34,7 @@ public class StudentGradeTest{
 
     @Test
     public void testThatAverageOfStudentScoresWithZerosGivesAccurateResult(){
-        int[] totals      = {134};
+        int[] totals= {134};
         double[] averages = StudentGrade.calculateAverage(totals, 3);
         assertEquals(44.67, averages[0], 0.01);
     }
@@ -48,7 +48,7 @@ public class StudentGradeTest{
     }
 
     @Test
-    public void testThatPositionsAreCorrectlyAssigned(){
+    public void testThatIfIPassStudentTotalItWillGiveTheCorrectPositionOfTheStudent(){
         int[] totals = {137, 216, 154, 227};
         int[] positions = StudentGrade.findPositions(totals);
         assertEquals(4, positions[0]);
@@ -65,5 +65,93 @@ public class StudentGradeTest{
         assertEquals(2, positions[1]);
         assertEquals(1, positions[2]);
     }
+   @Test
+    public void testThatIfIPassScoresForAllSubjectsItGivesCorrectTotal(){
+        int[][] scores = {{67, 21, 49}, {98, 62, 56}, {93, 34, 27}, {78, 83, 66}};
+        assertEquals(336, StudentGrade.calculateSubjectTotal(scores, 0));
+        assertEquals(200, StudentGrade.calculateSubjectTotal(scores, 1));
+        assertEquals(198, StudentGrade.calculateSubjectTotal(scores, 2));
+    }
 
+    @Test
+    public void testThatIfIPassScoresForAllSubjectsItGivesCorrectAverage(){
+        int[][] scores = {{67, 21, 49}, {98, 62, 56}, {93, 34, 27}, {78, 83, 66}};
+        assertEquals(84.00, StudentGrade.calculateSubjectAverage(scores, 0), 0.01);
+        assertEquals(50.00, StudentGrade.calculateSubjectAverage(scores, 1), 0.01);
+        assertEquals(49.50, StudentGrade.calculateSubjectAverage(scores, 2), 0.01);
+    }
+
+    @Test
+    public void ThatIfIPassAllScoresForAllStudentsIWillFindTheHighestScoringStudent(){
+        int[][] scores = {{67, 21, 49}, {98, 62, 56}, {93, 34, 27}, {78, 83, 66}};
+        int[] result   = StudentGrade.findHighestScoringStudent(scores, 0);
+        assertEquals(1, result[0]);
+        assertEquals(98, result[1]);
+    }
+
+    @Test
+    public void ThatIfIPassAllScoresForAllStudentsIWillFindTheLowestScoringStudent(){
+        int[][] scores = {{67, 21, 49}, {98, 62, 56}, {93, 34, 27}, {78, 83, 66}};
+        int[] result   = StudentGrade.findLowestScoringStudent(scores, 0);
+        assertEquals(0, result[0]);
+        assertEquals(67, result[1]);
+    }
+
+    @Test
+    public void ThatIfIPassAllScoresForAllSubjectsIWillFindTheHardestSubject(){
+        int[][] scores = {{67, 21, 49}, {98, 62, 56}, {93, 34, 27}, {78, 83, 66}};
+        int[] result   = StudentGrade.findHardestSubject(scores);
+        assertEquals(1, result[0]); 
+        assertEquals(2, result[1]);
+    }
+
+    @Test
+    public void ThatIfIPassAllScoresForAllSubjectsIWillFindTheEasiestSubject(){
+        int[][] scores = {{67, 21, 49}, {98, 62, 56}, {93, 34, 27}, {78, 83, 66}};
+        int[] result   = StudentGrade.findEasiestSubject(scores);
+        assertEquals(0, result[0]); 
+        assertEquals(4, result[1]); 
+    }
+
+    @Test
+    public void ThatIfIPassAllScoresForAllStudentsIWillFindTheOverallHighestScore(){
+        int[][] scores = {{67, 21, 49}, {98, 62, 56}, {93, 34, 27}, {78, 83, 66}};
+        int[] result   = StudentGrade.findOverallHighest(scores);
+        assertEquals(1, result[0]);
+        assertEquals(0, result[1]); 
+        assertEquals(98, result[2]);
+    }
+
+    @Test
+    public void testThatIfIPassAllScoresForAllStudentsIWillFindTheOverallLowestScore(){
+        int[][] scores = {{67, 21, 49}, {98, 62, 56}, {93, 34, 27}, {78, 83, 66}};
+        int[] result   = StudentGrade.findOverallLowest(scores);
+        assertEquals(0, result[0]);
+        assertEquals(1, result[1]); 
+        assertEquals(21, result[2]); 
+    }
+
+    @Test
+    public void testThatIfIPassTheTotalsOfEachStudentIWillGetTheBestStudent(){
+        int[] totals = {137, 216, 154, 227};
+        assertEquals(3, StudentGrade.findBestStudent(totals));
+    }
+
+    @Test
+    public void testThatIfIPassTheTotalsOfEachStudentScoresIWillFindTheWorstStudent(){
+        int[] totals = {137, 216, 154, 227};
+        assertEquals(0, StudentGrade.findWorstStudent(totals));
+    }
+
+    @Test
+    public void testThatIfIPassTheTotalsOfEachStudentScoresIWillGetCorrectTotal(){
+        int[] totals = {137, 216, 154, 227};
+        assertEquals(734, StudentGrade.calculateClassTotal(totals));
+    }
+
+    @Test
+    public void testThatIfIPassTheTotalsOfEachStudentScoresIWillGetCorrectClassAverage(){
+        int[] totals = {137, 216, 154, 227};
+        assertEquals(183.5, StudentGrade.calculateClassAverage(totals), 0.01);
+    }
 }
